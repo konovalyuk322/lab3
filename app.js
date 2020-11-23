e1372b3facc9361b034b6578b4958ff6def2155c
t1mer <konovalyuk322@gmail.com>
2020-11-23 11:15:37 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+(function () {
+    'use strict';
+
+    angular.module('App', [])
+        .controller('AppController', AppController);
+
+    AppController.$inject = ['$scope'];
+
+    function AppController($scope){
+        $scope.text = "";
+        $scope.list = "";
+
+        $scope.showAnswer = function (){
+            $scope.text = result($scope.list);
+        }
+
+        function result(elem1){
+            let elem = elem1.split(',') || elem1.split(' ');
+
+            for (let i = elem.length - 1; i >= 0; i--){
+                if (elem[i].trim() === "") elem.splice(i, 1);
+            }
+            if (elem.length === 0){
+                return"Будь ласка, уведіть дані!";
+            } 
+            else if (elem.length < 4){
+                return "На здоров'я!";
+            } 
+            else {
+                return "Це забагато!";
+            }
+        }
+    }
+})();
